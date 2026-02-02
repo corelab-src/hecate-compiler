265a3dd963cb6c7b9f8c4c32d0861a520dc271e5
seonyoung <seonyoung@yonsei.ac.kr>
2026-02-02 17:03:17 +0900

    [Fix] Fix Installing Error in install script

--- install.sh
+++ install.sh
@@ -2,14 +2,12 @@
 echo "Installing Hecate Python packages"
 
 cd $HECATE/python/hecate
-python setup.py sdist --format=tar
 pip uninstall -y hecate 2>/dev/null || true
-pip install dist/hecate-0.0.1.tar
+pip install -e . 
 
 cd $HECATE/python/poly
-python setup.py sdist --format=tar
 pip uninstall -y poly 2>/dev/null || true
-pip install dist/poly-0.0.1.tar
+pip install -e . 
 
 cd $HECATE/python/hetorch
 pip uninstall -y hetorch 2>/dev/null || true
